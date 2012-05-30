67d64b252d30151ddfabd5f83295f8c9380e05ca
Muerdter, Nick <nick.muerdter@nrel.gov>
2012-05-30 20:45:00 +0000

    Make source explict when installing custom gem, so it will work on first run, rather than having to wait until second run when the system gemrc file is read.

--- recipes/auto_checkin.rb
+++ recipes/auto_checkin.rb
@@ -11,4 +11,5 @@ include_recipe "rbenv::global_version"
 
 rbenv_gem "svn_auto_checkin" do
   ruby_version node[:rbenv][:install_global_version]
+  options "--source 'http://gems.cttsdev.nrel.gov'"
 end
